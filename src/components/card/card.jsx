339fc6637f7515e4128eb998b868e2d638593cda
seoulsaram <kaste2233@gmail.com>
2021-02-22 14:36:20 +0900

    add card and edit form component

---
+++ src/components/card/card.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./card.module.css";
+
+const DEFAULT_IMAGE = "/images/default_logo.png";
+const Card = ({ card }) => {
+  const { name, company, title, email, message, theme, fileName, fileURL } = card;
+  //card안에 있는 key를 변수로 선언하고 card를 넣어주면 각각 이름에 맞는 값들이 알아서 들어간다.
+  const url = fileURL || DEFAULT_IMAGE;
+  //fileURL이 없다면 DEFAULT_IMAGE를 사용한다.
+
+  return (
+    <li className={`${styles.card} ${getStyles(theme)}`}>
+      <img className={styles.avatar} src={url} alt="profile" />
+      <div className={styles.info}>
+        <h1 className={styles.name}>{name}</h1>
+        <p className={styles.company}>{company}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.message}>{message}</p>
+      </div>
+    </li>
+  );
+};
+
+function getStyles(theme) {
+  switch (theme) {
+    case "dark":
+      return styles.dark;
+    case "light":
+      return styles.light;
+    case "colorful":
+      return styles.colorful;
+    default:
+      throw new Error(`unknown theme: ${theme}`);
+  }
+}
+
+export default Card;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -8,6 +8,41 @@ import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
   const history = useHistory();
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "ellie",
+      company: "samsung",
+      theme: "light",
+      title: "software engineer",
+      email: "kaste3@naver.com",
+      message: "go for it",
+      filenam: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "ellie",
+      company: "samsung",
+      theme: "colorful",
+      title: "software engineer",
+      email: "kaste3@naver.com",
+      message: "go for it",
+      filenam: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "ellie",
+      company: "samsung",
+      theme: "dark",
+      title: "software engineer",
+      email: "kaste3@naver.com",
+      message: "go for it",
+      filenam: "ellie",
+      fileURL: "ellie.png",
+    },
+  ]);
   const onLogout = () => {
     authService.logout();
   };
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
